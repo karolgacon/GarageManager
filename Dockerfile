3ace775fa036c9b91a80f79669c7e7bd1e734e1a
karolgacon <karolgacon22@gmail.com>
2025-03-05 17:11:28 +0100

    Added PostgreSQL to Docker

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,17 @@ FROM python:3.13.0-alpine3.20
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
+
+ENV PATH="/py/bin:$PATH"
+RUN python -m venv /py && \
+    pip install --upgrade pip && \
+    apk add --update --upgrade --no-cache postgresql-client && \
+    apk add --update --upgrade --no-cache --virtual .tmp \
+        build-base postgresql-dev
+
+RUN pip install -r /requirements.txt && apk del .tmp
+
 COPY ./backend /backend
 WORKDIR /backend
 
-RUN python -m venv env /py && \
-    /py/bin/pip install --upgrade pip && \
-    /py/bin/pip install -r /requirements.txt
-
-ENV PATH="/py/bin:$PATH"
-
-CMD ["python","manage.py","runserver","0.0.0.0:8000"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINIE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSW"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': '5432',
     }
 }
 
