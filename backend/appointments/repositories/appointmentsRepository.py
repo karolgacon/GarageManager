af9e34e700ec4cb19a6ef3b940940000ca86c7c0
karolgacon <karolgacon22@gmail.com>
2025-05-04 18:09:33 +0200

    refactor(repository): refactor code to stick to DRY

--- backend/appointments/repositories/appointmentsRepository.py
+++ backend/appointments/repositories/appointmentsRepository.py
@@ -1,96 +1,9 @@
 from ..models import Appointment
+from backend.repositories.baseRepository import BaseRepository
 
-class AppointmentRepository:
-    @staticmethod
-    def get_all_appointments():
-        """
-        Retrieves all appointments from the database.
-        """
-        try:
-            appointments = Appointment.objects.all()
-        except Exception as e:
-            # Log the exception or handle it as needed
-            raise RuntimeError(f"Error retrieving appointments: {str(e)}")
-        return appointments
-    
-    @staticmethod
-    def create_appointment(data):
-        """
-        Creates a new appointment in the database.
-        """
-        try:
-            # Validate data here if needed
-            appointment = Appointment(**data)
-            appointment.save()
-        except Exception as e:
-            # Log the exception or handle it as needed
-            raise RuntimeError(f"Error creating appointment: {str(e)}")
-        # Return the created appointment object
-        return appointment
+class AppointmentRepository(BaseRepository):
+    model = Appointment
 
-    @staticmethod
-    def get_appointment_by_id(appointment_id):
-        """
-        Retrieves an appointment by its ID.
-        """
-        try:
-            appointment = Appointment.objects.filter(id=appointment_id).first()
-        except Exception as e:
-            # Log the exception or handle it as needed
-            raise RuntimeError(f"Error retrieving appointment: {str(e)}")
-        if not appointment:
-            raise ValueError(f"Appointment with ID {appointment_id} does not exist.")
-        return appointment
-
-    @staticmethod
-    def delete_appointment(appointment_id):
-        """
-        Deletes an appointment by its ID.
-        """
-        try:
-            appointment = Appointment.objects.filter(id=appointment_id).first()
-        except Exception as e:
-            # Log the exception or handle it as needed
-            raise RuntimeError(f"Error deleting appointment: {str(e)}")
-        if not appointment:
-            raise ValueError(f"Appointment with ID {appointment_id} does not exist.")
-        appointment.delete()
-        return True
-    
-    @staticmethod
-    def update_appointment(appointment_id, **data):
-        """
-        Updates an existing appointment.
-        """
-        try:
-            appointment = Appointment.objects.filter(id=appointment_id).first()
-        except Exception as e:
-            # Log the exception or handle it as needed
-            raise RuntimeError(f"Error updating appointment: {str(e)}")
-        if not appointment:
-            raise ValueError(f"Appointment with ID {appointment_id} does not exist.")
-        for key, value in data.items():
-            setattr(appointment, key, value)
-        appointment.save()
-        return appointment
-    
-    @staticmethod
-    def partially_update_appointment(appointment_id, **data):
-        """
-        Partially updates an existing appointment.
-        """
-        try:
-            appointment = Appointment.objects.filter(id=appointment_id).first()
-        except Exception as e:
-            # Log the exception or handle it as needed
-            raise RuntimeError(f"Error partially updating appointment: {str(e)}")
-        if not appointment:
-            raise ValueError(f"Appointment with ID {appointment_id} does not exist.")
-        for key, value in data.items():
-            setattr(appointment, key, value)
-        appointment.save()
-        return appointment
-    
     @staticmethod
     def get_appointments_by_client(client_id):
         """
@@ -98,13 +11,12 @@ class AppointmentRepository:
         """
         try:
             appointments = Appointment.objects.filter(client_id=client_id)
+            if not appointments.exists():
+                raise ValueError(f"No appointments found for client with ID {client_id}.")
+            return appointments
         except Exception as e:
-            # Log the exception or handle it as needed
             raise RuntimeError(f"Error retrieving appointments for client {client_id}: {str(e)}")
-        if not appointments:
-            raise ValueError(f"No appointments found for client with ID {client_id}.")
-        return appointments
-    
+
     @staticmethod
     def get_appointments_by_workshop(workshop_id):
         """
@@ -112,12 +24,12 @@ class AppointmentRepository:
         """
         try:
             appointments = Appointment.objects.filter(workshop_id=workshop_id)
+            if not appointments.exists():
+                raise ValueError(f"No appointments found for workshop with ID {workshop_id}.")
+            return appointments
         except Exception as e:
-            # Log the exception or handle it as needed
             raise RuntimeError(f"Error retrieving appointments for workshop {workshop_id}: {str(e)}")
-        if not appointments:
-            raise ValueError(f"No appointments found for workshop with ID {workshop_id}.")  
-        return appointments
+
     @staticmethod
     def get_appointments_by_vehicle(vehicle_id):
         """
@@ -125,13 +37,12 @@ class AppointmentRepository:
         """
         try:
             appointments = Appointment.objects.filter(vehicle_id=vehicle_id)
+            if not appointments.exists():
+                raise ValueError(f"No appointments found for vehicle with ID {vehicle_id}.")
+            return appointments
         except Exception as e:
-            # Log the exception or handle it as needed
             raise RuntimeError(f"Error retrieving appointments for vehicle {vehicle_id}: {str(e)}")
-        if not appointments:
-            raise ValueError(f"No appointments found for vehicle with ID {vehicle_id}.")
-        return appointments
-    
+
     @staticmethod
     def get_appointments_by_status(status):
         """
@@ -139,13 +50,12 @@ class AppointmentRepository:
         """
         try:
             appointments = Appointment.objects.filter(status=status)
+            if not appointments.exists():
+                raise ValueError(f"No appointments found with status {status}.")
+            return appointments
         except Exception as e:
-            # Log the exception or handle it as needed
             raise RuntimeError(f"Error retrieving appointments with status {status}: {str(e)}")
-        if not appointments:
-            raise ValueError(f"No appointments found with status {status}.")
-        return appointments
-    
+
     @staticmethod
     def get_appointments_by_priority(priority):
         """
@@ -153,9 +63,8 @@ class AppointmentRepository:
         """
         try:
             appointments = Appointment.objects.filter(priority=priority)
+            if not appointments.exists():
+                raise ValueError(f"No appointments found with priority {priority}.")
+            return appointments
         except Exception as e:
-            # Log the exception or handle it as needed
             raise RuntimeError(f"Error retrieving appointments with priority {priority}: {str(e)}")
-        if not appointments:
-            raise ValueError(f"No appointments found with priority {priority}.")
-        return appointments
